d0268b342f446d5681dd7b1df6ea0a880a162e27
kashifhussainpathan <121045014+kashifhussainpathan@users.noreply.github.com>
2022-12-24 21:41:41 +0530

    Added features and related to styling

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-var btnTranslate = document.querySelector("#btn-translate");
-var txtinput = document.querySelector("#txt-input");
-var outputDiv= document.querySelector("#output")
+var btnTranslate = document.getElementById("btn-translate");
+var txtinput = document.getElementById("txt-input");
+var outputDiv = document.getElementById("output")
+
+var serverURL = "https://api.funtranslations.com/translate/valyrian.json"
+
+getTranslatedUrl = (text) => serverURL + "?" + "text=" + text;
+
+errorHandler = (error) => {
+    console.log('Error Occured', error);
+    alert("Something is wroong with server! try again after some time")
+}
 
 btnTranslate.addEventListener("click", eventHandler)
- function eventHandler(){
-    outputDiv.innerText ="asassssas" + txtinput.value;
-    
- };
- ;
+
+function eventHandler() {
+    inputText = txtinput.value;
+    fetch(getTranslatedUrl(inputText))
+        .then(Response => Response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
+};
