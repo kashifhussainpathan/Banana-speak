c8c171a4b79d442ba4170c4de98f19542a6cf1d4
kashifhussainpathan <121045014+kashifhussainpathan@users.noreply.github.com>
2022-12-24 16:48:56 +0530

    input output processing done

---
+++ app.js
@@ -0,0 +1,10 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtinput = document.querySelector("#txt-input");
+var outputDiv= document.querySelector("#output")
+
+btnTranslate.addEventListener("click", eventHandler)
+ function eventHandler(){
+    outputDiv.innerText ="asassssas" + txtinput.value;
+    
+ };
+ ;
